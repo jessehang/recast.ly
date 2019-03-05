7cf045297f42e61ec8d3a8b5a6ad16719956ef88
jessehang <hangtimee@gmail.com>
2019-03-04 17:42:40 -0800

    Implemented Video list and Video player

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import allData from '../data/exampleVideoData.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -7,14 +11,17 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><VideoPlayer video={allData[0]}/></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <div>
+          <VideoList videos={allData}/>
+        </div>
       </div>
     </div>
   </div>
 );
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
