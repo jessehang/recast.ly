1f7ef66a99b45b849df06c81ad50b7ebe35ad0f5
Jesse Hang <hangtimee@gmail.com>
2019-03-10 22:28:04 -0700

    Implemented fully working reacst.ly product

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({videos, handleVideoClick}) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videos.map(video => <VideoListEntry video={video} handleVideoClick={handleVideoClick}/>)}
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+import youTubeAPI from './config/youtube.js';
+import searchYouTube from './lib/searchYouTube.js';
 
+ReactDOM.render(<App API_KEY={youTubeAPI} searchYouTube={searchYouTube}/>, document.getElementById('app'));
